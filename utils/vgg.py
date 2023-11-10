d4a9746737ccfe2f3ccce703b55c608763d23cd8
Woo-Joo Choi <143667937+dkifty@users.noreply.github.com>
2023-11-10 13:08:21 +0900

    Update vgg.py

--- utils/vgg.py
+++ utils/vgg.py
@@ -1,12 +1,11 @@
 def vgg_block(inputs, num_cnn=3, filters=64):
-    global model
-    
     model = inputs
     
     for cnn_num in range(num_cnn):
         model = tf.keras.layers.Conv2D(filters=filters, kernel_size=(3,3), padding='same')(model)
         model = tf.keras.layers.BatchNormalization()(model)
         model = tf.keras.layers.Activation('relu')(model)
+        
     model = tf.keras.layers.MaxPooling2D(pool_size=(2,2), strides=2)(model)
     
     return model
@@ -21,25 +20,25 @@ def vgg(model_name = 'vgg16', input_shape = (500, 500, 3)):
         num_cnn_list = [1,1,4,4,4]
         filter_list = [64, 128, 256, 512, 512]
         for num_cnn, filters in zip(num_cnn_list, filter_list):
-            vgg_block(model, num_cnn = num_cnn, filters=filters)
+            model = vgg_block(model, num_cnn = num_cnn, filters=filters)
             
     elif model_name == 'vgg13':
         num_cnn_list = [2,2,2,2,2]
         filter_list = [64, 128, 256, 512, 512]
         for num_cnn, filters in zip(num_cnn_list, filter_list):
-            vgg_block(model, num_cnn = num_cnn, filters=filters)
+            model = vgg_block(model, num_cnn = num_cnn, filters=filters)
             
     elif model_name == 'vgg16':
         num_cnn_list = [2,2,3,3,3]
         filter_list = [64, 128, 256, 512, 512]
         for num_cnn, filters in zip(num_cnn_list, filter_list):
-            vgg_block(model, num_cnn = num_cnn, filters=filters)
+            model = vgg_block(model, num_cnn = num_cnn, filters=filters)
             
     elif model_name == 'vgg19' or model_name == 'vgg':
         num_cnn_list = [2,2,4,4,4]
         filter_list = [64, 128, 256, 512, 512]
         for num_cnn, filters in zip(num_cnn_list, filter_list):
-            vgg_block(model, num_cnn = num_cnn, filters=filters)
+            model = vgg_block(model, num_cnn = num_cnn, filters=filters)
     
     else:
         pass    
