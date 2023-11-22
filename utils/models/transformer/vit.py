958e9061b1b42fd66e7aa68d5fa0165eabe10aec
Woo-Joo Choi <143667937+dkifty@users.noreply.github.com>
2023-11-22 14:25:16 +0900

    Update vit.py

--- utils/models/transformer/vit.py
+++ utils/models/transformer/vit.py
@@ -1,8 +1,8 @@
 # vit
 def mlp(x, hidden_units, dropout_rate):
     for units in hidden_units:
-        x = layers.Dense(units, activation=tf.nn.gelu)(x)
-        x = layers.Dropout(dropout_rate)(x)
+        x = tf.keras.layers.Dense(units, activation=tf.nn.gelu)(x)
+        x = tf.keras.layers.Dropout(dropout_rate)(x)
     return x
 
 class Patches(tf.keras.layers.Layer):
